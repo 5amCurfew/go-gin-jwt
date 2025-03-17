87e4adc4d4d2d2eef51572594808bea41ba4026e
5amCurfew <samueltobyknight@gmail.com>
2025-03-17 15:23:15 +0000

    user.go, explicit ctrl file names reflecting route

--- models/user.go
+++ models/user.go
@@ -1,12 +1,19 @@
 package models
 
 import (
+	"fmt"
 	"html"
+	"os"
 	"strings"
 
+	log "github.com/sirupsen/logrus"
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
+// gorm Object Database
+var DB *gorm.DB
 
 // User model
 type User struct {
@@ -14,6 +21,21 @@ type User struct {
 	Email    string `gorm:"size:255;not null;unique" json:"email"`
 	Password string `gorm:"size:255;not null;" json:"password"`
 	IsAdmin  bool   `gorm:"default:false" json:"is_admin"`
+}
+
+// Connect
+func ConnectToAuthDatabase() {
+	var err error
+
+	databaseName := os.Getenv("DATABASE_NAME")
+	DB, err = gorm.Open(sqlite.Open(databaseName), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(fmt.Sprintf("failed to connect database %s", databaseName))
+	} else {
+		log.Infof("%s connection successful", databaseName)
+	}
+
+	DB.AutoMigrate(&User{})
 }
 
 // Authenticate user comparing candidate password with recorded (hashed) password
